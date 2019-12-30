e085335879727213d472bdc56bbe6940649957dd
juraj.orencak <juraj.orencak@ataccama.com>
2019-12-30 20:50:41 +0100

    maintenance

---
+++ Factory/src/main/java/cvut/fel/omo/factory/maintenance/Maintainer.java
@@ -0,0 +1,12 @@
+package cvut.fel.omo.factory.maintenance;
+
+public class Maintainer {
+
+    int wage;
+    boolean availability;
+
+    public Maintainer(int wage){
+        this.availability = true;
+        this.wage = wage;
+    }
+}
