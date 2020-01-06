fc86be9a3b00b8c81a33062431fb4e0889fc81fc
Štěpán <vejvoste@fel.cvut.cz>
2020-01-06 19:21:37 +0100

    main

---
+++ Factory/src/main/java/cvut/fel/omo/factory/Main.java
@@ -0,0 +1,18 @@
+package cvut.fel.omo.factory;
+
+import cvut.fel.omo.factory.management.Blueprint;
+
+public class Main {
+    public static void main(String args[]){
+        Time time = Time.getInstance();
+
+        Factory factory = new Factory();
+        factory.addMaterial("metal",10,500);
+        String[] materials = {"metal", "wood"};
+        int[] numMaterials = {20,10};
+        Blueprint b = new Blueprint("RRMPPMR",materials, numMaterials,10);
+        factory.addBlueprint(b);
+        factory.createLine(b,1,200);
+
+    }
+}
