5b781b819681fe6b93382974ce4c547182ada3df
Štěpán <vejvoste@fel.cvut.cz>
2020-01-16 23:09:59 +0100

    completed consumption report

--- Factory/src/main/java/cvut/fel/omo/factory/report/ConsumptionReport.java
+++ Factory/src/main/java/cvut/fel/omo/factory/report/ConsumptionReport.java
@@ -17,44 +17,66 @@ public class ConsumptionReport implements Report {
     @Override
     public void create(int from, int to) {
         try {
-            File f = new File("consumptionReport_from_"+from+"_to_"+to+".txt");
+            File f = new File("consumptionReport_from_" + from + "_to_" + to + ".txt");
             PrintWriter writer = new PrintWriter(f, StandardCharsets.UTF_8);
-            writer.write("CONSUMPTION REPORT - FROM TACT "+from+" TO TACT "+to+"\n");
+            writer.write("CONSUMPTION REPORT - FROM TACT " + from + " TO TACT " + to + "\n");
             writer.flush();
             //DATA
             int total = 0;
+            int size = -1;
             ArrayList<Integer> line_consumption = new ArrayList<Integer>();
+            ArrayList<Integer> line_robot_consumption = new ArrayList<Integer>();
+            ArrayList<Integer> line_machine_consumption = new ArrayList<Integer>();
+            ArrayList<ArrayList<Integer>> robot_serial = new ArrayList<ArrayList<Integer>>();
+            ArrayList<ArrayList<Integer>> machine_serial = new ArrayList<ArrayList<Integer>>();
+            ArrayList<ArrayList<Integer>> every_machine = new ArrayList<ArrayList<Integer>>();
+            ArrayList<ArrayList<Integer>> every_robot = new ArrayList<ArrayList<Integer>>();
             /////////////////////////////////////////////
-            for(int i=from;i<=to;i++){
+            for (int i = from; i <= to; i++) {
                 StatData statData = archive.getStatData().get(i);
                 total += statData.getFactoryElectricity();
-                for(int j=0;j<statData.getLineData().size();j++){
-                    LineStatData lineData = statData.getLineData().get(j);
-                }
-            }
-            for(int i=from;i<=to;i++) {
-                StatData statData = archive.getStatData().get(i);
-
-                writer.write("\tTotal consumption: " + total + "\n");
-                writer.flush();
                 for (int j = 0; j < statData.getLineData().size(); j++) {
                     LineStatData lineData = statData.getLineData().get(j);
-                    writer.write("\t+++++++++++++++++++++++++++++\n" +
-                            "\tTotal consumption of line " + (j + 1) + ": "+lineData.getAllElectricity()+"\n" +
-                            "\t\tConsumption of all machines: "+lineData.getMachinesElectricity()+"\n"+
-                            "\t\tConsumption of all robots: " +lineData.getRobotsElectricity()+"\n"+
-                            "\t\tConsumption of every machine: \n");
-                    writer.flush();
-                    for(int x=0;x<lineData.getMachElectricity().size();x++){
-                        writer.write("\t\t\t"+lineData.getMachElectricity().get(x)+"\n");
-                        writer.flush();
+                    if (j > size) {
+                        line_machine_consumption.add(lineData.machinesElectricity);
+                        line_robot_consumption.add(lineData.robotsElectricity);
+                        line_consumption.add(lineData.allElectricity);
+                        every_machine.add(lineData.machElectricity);
+                        every_robot.add(lineData.robElectricity);
+                        robot_serial.add(lineData.robotSerial);
+                        machine_serial.add(lineData.machineSerial);
+                        size++;
+                    } else {
+                        line_machine_consumption.set(j, lineData.machinesElectricity + line_machine_consumption.get(j));
+                        line_robot_consumption.set(j, lineData.robotsElectricity + line_robot_consumption.get(j));
+                        line_consumption.set(j, lineData.allElectricity + line_consumption.get(j));
+                        for (int x = 0; x < every_machine.get(j).size(); x++) {
+                            every_machine.get(j).set(x, lineData.machElectricity.get(x) + every_machine.get(j).get(x));
+                        }
+                        for (int x = 0; x < every_robot.get(j).size(); x++) {
+                            every_robot.get(j).set(x, lineData.robElectricity.get(x) + every_robot.get(j).get(x));
+                        }
                     }
-                    writer.write("\t\tConusmption of every robot: \n");
+                }
+            }
+            writer.write("\tTotal consumption: " + total + "\n");
+            writer.flush();
+            for (int j = 0; j < line_consumption.size(); j++) {
+                writer.write("\t+++++++++++++++++++++++++++++\n" +
+                        "\tTotal consumption of line " + (j + 1) + ": " + line_consumption.get(j) + "\n" +
+                        "\t\tConsumption of all machines: " + line_machine_consumption.get(j) + "\n" +
+                        "\t\tConsumption of all robots: " + line_robot_consumption.get(j) + "\n" +
+                        "\t\tConsumption of every machine: \n");
+                writer.flush();
+                for (int x = 0; x < every_machine.get(j).size(); x++) {
+                    writer.write("\t\t\tSerial number: " + machine_serial.get(j).get(x) + " Consumption: " + every_machine.get(j).get(x) + "\n");
                     writer.flush();
-                    for(int x=0;x<lineData.getRobElectricity().size();x++){
-                        writer.write("\t\t\t"+lineData.getRobElectricity().get(x)+"\n");
-                        writer.flush();
-                    }
+                }
+                writer.write("\t\tConsumption of every robot: \n");
+                writer.flush();
+                for (int x = 0; x < every_robot.get(j).size(); x++) {
+                    writer.write("\t\t\tSerial number: " + robot_serial.get(j).get(x) + " Consumption: " + every_robot.get(j).get(x) + "\n");
+                    writer.flush();
                 }
             }
             writer.close();
--- Factory/src/main/java/cvut/fel/omo/factory/report/LineStatData.java
+++ Factory/src/main/java/cvut/fel/omo/factory/report/LineStatData.java
@@ -10,8 +10,10 @@ public class LineStatData{
     int allElectricity;
     int robotsElectricity;
     int machinesElectricity;
-    ArrayList<String> robElectricity;
-    ArrayList<String> machElectricity;
+    ArrayList<Integer> robElectricity;
+    ArrayList<Integer> robotSerial;
+    ArrayList<Integer> machElectricity;
+    ArrayList<Integer> machineSerial;
     int wages;
 
 
@@ -19,28 +21,34 @@ public class LineStatData{
         this.allElectricity=0;
         this.robotsElectricity = 0;
         this.machinesElectricity = 0;
-        robElectricity = new ArrayList<String>();
-        machElectricity = new ArrayList<String>();
+        robElectricity = new ArrayList<Integer>();
+        machElectricity = new ArrayList<Integer>();
+        robotSerial = new ArrayList<Integer>();
+        machineSerial = new ArrayList<Integer>();
     }
 
     public int getAllElectricity(){return this.allElectricity;}
     public int getRobotsElectricity(){return this.robotsElectricity;}
     public int getMachinesElectricity(){return this.machinesElectricity;}
-    public ArrayList<String> getRobElectricity(){return this.robElectricity;}
-    public ArrayList<String> getMachElectricity(){return this.machElectricity;}
+    public ArrayList<Integer> getRobElectricity(){return this.robElectricity;}
+    public ArrayList<Integer> getMachElectricity(){return this.machElectricity;}
+    public ArrayList<Integer> getRobotSerial() { return robotSerial; }
+    public ArrayList<Integer> getMachineSerial() { return machineSerial; }
 
     public void update(Line line){
         for(Robot r:line.getRobots()){
             RobotAPI API = r.getAPI();
             this.allElectricity += API.getElectricity();
             this.robotsElectricity += API.getElectricity();
-            this.robElectricity.add("Serial number: "+r.getSerialNumber()+" Consumption this tact: "+API.getElectricity());
+            this.robotSerial.add(r.getSerialNumber());
+            this.robElectricity.add(API.getElectricity());
         }
         for(Machine m: line.getMachines()){
             MachineAPI API = m.getAPI();
             this.allElectricity += API.getElectricity();
             this.machinesElectricity += API.getElectricity();
-            this.machElectricity.add("Serial number: "+m.getSerialNumber()+" Consumption this tact: "+API.getElectricity());
+            this.machineSerial.add(m.getSerialNumber());
+            this.machElectricity.add(API.getElectricity());
         }
     }
 
